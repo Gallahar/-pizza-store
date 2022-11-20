3d5ed88607b223fad28ce58e32f0c7c3937a2aed
Gallahar <david.kushnir.d@gmail.com>
2022-11-20 05:12:52 +0300

    correct typing for action&dispatch qs parsed string

--- src/redux/slices/filterSlice.ts
+++ src/redux/slices/filterSlice.ts
@@ -1,6 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootStore } from "../store";
 
-const initialState = {
+export type TSort = {
+  name: "популярности" | "цене" | "алфавиту";
+  sort: "rating" | "price" | "title";
+};
+
+export interface IFilterSliceState {
+  pagination: number;
+  categoryIndex: number;
+  sort: TSort;
+  searchInput: string;
+  order: boolean;
+}
+
+const initialState: IFilterSliceState = {
   pagination: 1,
   categoryIndex: 0,
   sort: {
@@ -15,30 +29,30 @@ const filterSlice = createSlice({
   name: "filter",
   initialState,
   reducers: {
-    setCategoryIndex: (state, action) => {
+    setCategoryIndex: (state, action: PayloadAction<number>) => {
       state.categoryIndex = action.payload;
     },
-    setFilterOrder: (state, action) => {
+    setFilterOrder: (state, action: PayloadAction<TSort>) => {
       state.sort = action.payload;
     },
-    setSearch: (state, action) => {
+    setSearch: (state, action: PayloadAction<string>) => {
       state.searchInput = action.payload;
     },
-    setPagination: (state, action) => {
+    setPagination: (state, action: PayloadAction<number>) => {
       state.pagination = action.payload;
     },
-    setParams: (state, action) => {
+    setParams: (state, action: PayloadAction<IFilterSliceState>) => {
       state.sort = action.payload.sort;
       state.categoryIndex = Number(action.payload.categoryIndex);
       state.pagination = Number(action.payload.pagination);
     },
-    setOrder: (state, action) => {
+    setOrder: (state, action: PayloadAction<boolean>) => {
       state.order = action.payload;
     },
   },
 });
 
-export const selectFilter = (state) => state.sorting;
+export const selectFilter = (state: RootStore) => state.sorting;
 
 export const {
   setCategoryIndex,
