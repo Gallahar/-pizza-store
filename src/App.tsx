421083edab06deb4de5ae701c05e6cd4404b4333
Gallahar <david.kushnir.d@gmail.com>
2022-11-24 17:39:37 +0300

    correct usage state from redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
 import PizzaInfo from "./pages/PizzaInfo/PizzaInfo";
 
 import "./scss/app.scss";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
-import CartEmpty from "./components/CartEmpty/CartEmpty";
 import MainLayout from "./components/Layouts/MainLayout";
-import { selectCartPizzas } from "./redux/slices/cartSlice";
 
 const App: React.FC = () => {
-  const { pizzas } = useSelector(selectCartPizzas);
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route
-          path="Cart"
-          element={pizzas.length > 0 ? <Cart /> : <CartEmpty />}
-        />
+        <Route path="Cart" element={<Cart />} />
         <Route path="*" element={<NotFound />} />
         <Route path="pizza/:id" element={<PizzaInfo />} />
       </Route>
